295917628de1f14d250afbc362a09bac379da198
Maciej Pająk <mpajak98@gmail.com>
2023-06-19 17:29:43 +0200

    fix(dqn): better implementation

--- dqn-learning.py
+++ dqn-learning.py
@@ -5,17 +5,36 @@ import numpy as np
 import pandas as pd
 import pygame
 from tqdm import tqdm
+import tensorflow as tf
 
 from src.game import Algorithm, Action
 from src.qlearning.dqnmodel import DQNModel
+
+
+
+def create_model():
+    inputs = tf.keras.layers.Input(shape=(500,500, 12))
+    x = tf.keras.layers.Resizing(32, 32)(inputs)
+    x = tf.keras.layers.Rescaling(127.5, offset=-1)(x)
+    x = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), padding='valid')(x)
+    # x = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), padding='valid')(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+
+    x = tf.keras.layers.Dense(16, activation='relu', kernel_initializer=tf.keras.initializers.HeUniform())(x)
+    x = tf.keras.layers.Dense(6, activation='linear', kernel_initializer=tf.keras.initializers.HeUniform())(x)
+    model = tf.keras.Model(inputs=[inputs], outputs=[x])
+    # print(model.summary())
+    return model
 
 
 def get_reward(player_alive, action, is_move_possible, suicide, kills, destroyed_boxes):
     r = -1/23
     if action == Action.NO_ACTION:
         r += -1/23
+    if not is_move_possible:
+        r += -1
     if not player_alive and not suicide:
-        r -= 100
+        r -= 300
     if not player_alive and suicide:
         r -= 500
     r += kills * 300
@@ -24,11 +43,17 @@ def get_reward(player_alive, action, is_move_possible, suicide, kills, destroyed
 
 
 if __name__ == "__main__":
-    model = DQNModel()
+    model = DQNModel(create_model())
+    learning_rate=0.01
     discount = 0.98
     episodes = 1000
-    model.compile(get_reward=get_reward,
-                  discount=discount)
+    model.compile(
+        loss=tf.keras.losses.Huber(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        metrics=['accuracy'],
+        get_reward=get_reward,
+        discount=discount,
+        learning_rate=0.001)
 
     grid_path = Path('.') / 'maps' / 'standard' / 'M.csv'
     grid = np.genfromtxt(grid_path, delimiter=',')
@@ -41,11 +66,15 @@ if __name__ == "__main__":
                    en3_alg=en3_alg,
                    box_density=(3, 6),
                    shuffle_positions=True,
-                   max_playing_time=120)
+                   max_playing_time=450)
 
     history = model.fit(episodes=episodes,
                         show_game=True,
-                        update_target_every=1000,
-                        batch_size=128,
+                        update_target_every=100,
+                        past_frames=4,
+                        batch_size=32,
+                        train_clip=(-100,100),
+                        save_every=2,
                         path_to_save='qtables/dqnmodels/modelM.h5',
-                        log_file='qtables/dqnmodels/log.csv')
+                        history_log='qtables/dqnmodels/history.csv',
+                        log_file='qtables/dqnmodels/log.log')
--- src/qlearning/dqnmodel.py
+++ src/qlearning/dqnmodel.py
@@ -9,44 +9,50 @@ from tqdm.auto import tqdm
 
 import tensorflow as tf
 import random
+from simple_chalk import green, red, yellow
 
 from ..game import Algorithm
 from ..game.game import Game
 from ..game.enums.action import Action
 
-def create_model(input_shape):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=input_shape),
-        tf.keras.layers.Conv2D(filters = 16, kernel_size=(3,3), padding='valid'),
-        tf.keras.layers.Conv2D(filters = 16, kernel_size=(3,3), padding='valid'),
-        tf.keras.layers.Conv2D(filters = 16, kernel_size=(3,3), padding='valid'),
-        tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(16, activation='relu', kernel_initializer=tf.keras.initializers.HeUniform()),
-        # tf.keras.layers.Dense(16, activation='relu', kernel_initializer=tf.keras.initializers.HeUniform()),
-        tf.keras.layers.Dense(6, activation='linear', kernel_initializer=tf.keras.initializers.HeUniform())
-    ])
-    model.compile(loss=tf.keras.losses.Huber(),
-                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-                  metrics=['accuracy'])
-    return model
-
 class DQNModel:
-    def __init__(self):
-        self.main_model: tf.keras.models.Model = None
-        self.target_model: tf.keras.models.Model = None
-        self.steps_to_update_target_model  = 0
+    def __init__(self, model):
+        self.main_model = tf.keras.models.clone_model(model)
+        self.main_model = model.__class__.from_config(model.get_config())
+
+        self.target_model = tf.keras.models.clone_model(model)
+        self.target_model = model.__class__.from_config(model.get_config())
+
+        self.target_model.set_weights(self.main_model.get_weights())
+        self.steps_to_update_target_model = 0
         self.update_target_every = 100
         self.batch_size = 128
+        self.log_file = None
+        self.past_frames = 4
+        self.verbosity = False
 
     def load(self, path: str) -> None:
         self.main_model = tf.keras.models.load_model(path)
         self.target_model = tf.keras.models.load_model(path)
 
     def compile(self,
+                loss,
+                optimizer,
+                metrics,
                 get_reward: Callable[[bool, Action, bool, bool, int, int], float],
-                discount=0.98,) -> None:
+                discount=0.98,
+                learning_rate=0.1) -> None:
         self.get_reward = get_reward
         self.discount = discount
+        self.learning_rate=learning_rate
+
+        self.main_model.compile(loss=loss,
+                      optimizer=optimizer,
+                      metrics=metrics)
+
+        self.target_model.compile(loss=loss,
+                      optimizer=optimizer,
+                      metrics=metrics)
 
     def set_game(self, grid: np.ndarray,
                  en1_alg: Algorithm,
@@ -66,22 +72,35 @@ class DQNModel:
     def fit(self,
             episodes: int = 1000,
             show_game: bool = False,
-            update_target_every: int=100,
-            batch_size: int=4,
+            update_target_every: int = 100,
+            past_frames:int = 4,
+            batch_size: int = 4,
+            train_clip = (-1,1),
+            save_every = 50,
+            show_every = 50,
             path_to_save: str = 'model.h5',
-            log_file: str = 'log.csv') -> pd.DataFrame:
+            history_log: str= 'history.csv',
+            log_file: str = 'log.log',
+            verbosity=False,
+            ) -> pd.DataFrame:
 
-        cols = ['epoch', 'epsilon', 'mean_reward','mean_loss', 'mean_acc']
+        self.verbosity = verbosity
+
+        cols = ['epoch', 'epsilon', 'mean_reward', 'mean_loss', 'mean_acc']
         history = pd.DataFrame(columns=cols)
-        history.to_csv(log_file, mode='a', index=False, header=cols)
+        history.to_csv(history_log, mode='w', index=False, header=cols)
 
-        self.main_model = create_model(input_shape=(11,11,2))
-        self.target_model = create_model(input_shape=(11,11,2))
-        self.target_model.set_weights(self.main_model.get_weights())
+        self.log_file = log_file
+        open(log_file, "w").close()
 
         self.batch_size = batch_size
+
         self.steps_to_update_target_model = 0
         self.update_target_every = update_target_every
+
+        self.past_frames = past_frames
+
+        self.train_clip = train_clip
 
         epsilon = 1
         max_epsilon = 1  # You can't explore more than 100% of the time
@@ -89,44 +108,50 @@ class DQNModel:
         decay = 0.01
 
         self.replay_memory = deque(maxlen=50_000)
-        for e in tqdm(range(episodes)):
+        for e in range(episodes):
             _, mean_reward, mean_loss, mean_acc = self.play_game(epsilon=epsilon, train=True, show=False)
-            if show_game and (e + 1) % 50 == 0:
-                self.play_game(epsilon=epsilon, train=False, show=True)
+            if show_game and (e + 1) % save_every == 0:
                 self.save(path_to_save)
-            print(f'Episode {e + 1}/{episodes} - epsilon: {epsilon:5.5f} - loss: {mean_loss:5.5f} - mean acc: {mean_acc:5.5f} - mean reward:{mean_reward:5.1f}')
+            if show_game and (e + 1) % show_every == 0:
+                self.play_game(epsilon=epsilon, train=False, show=True)
+            if self.verbosity:
+                print(f'Episode {e + 1}/{episodes} - epsilon: {epsilon:5.5f} - loss: {mean_loss:5.5f} - mean acc: {mean_acc:5.5f} - mean reward:{mean_reward:5.1f}\n')
+                print('\n' + '=' * 50)
+            with open(log_file, "a") as f:
+                f.write(f'Episode {e + 1}/{episodes} - epsilon: {epsilon:5.5f} - loss: {mean_loss:5.5f} - mean acc: {mean_acc:5.5f} - mean reward:{mean_reward:5.1f}\n')
+                f.write('\n' + '=' * 50 + '\n')
 
             epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * e)
 
             history = pd.DataFrame([[e, epsilon, mean_reward, mean_loss, mean_acc]], columns=cols)
-            history.to_csv(log_file, mode='a', index=False, header=False)
+            history.to_csv(history_log, mode='a', index=False, header=False)
 
         return history
 
-
     def train_model(self, replay_memory):
-        learning_rate = 0.1  # Learning rate
-
         MIN_REPLAY_SIZE = 1000
         if len(replay_memory) < MIN_REPLAY_SIZE:
-            return 0,0
+            return 0, 0
 
         mini_batch = random.sample(replay_memory, self.batch_size)
         current_states = np.array([transition[0] for transition in mini_batch])
-        current_qs_list = self.main_model.predict(current_states, verbose=0)
+        current_qs_list = self.main_model.predict(tf.convert_to_tensor(current_states, dtype=tf.float32), verbose=0)
         new_current_states = np.array([transition[3] for transition in mini_batch])
-        future_qs_list = self.target_model.predict(new_current_states, verbose=0)
+        future_qs_list = self.target_model.predict(tf.convert_to_tensor(new_current_states, dtype=tf.float32), verbose=0)
 
         X = []
         Y = []
         for index, (state, action, reward, future_state, alive) in enumerate(mini_batch):
+            if self.train_clip:
+                reward = np.clip(reward, self.train_clip[0], self.train_clip[1])
+
             if alive:
                 max_future_q = reward + self.discount * np.max(future_qs_list[index])
             else:
                 max_future_q = reward
 
             current_qs = current_qs_list[index]
-            current_qs[action] = (1 - learning_rate) * current_qs[action] + learning_rate * max_future_q
+            current_qs[action] = (1 - self.learning_rate) * current_qs[action] + self.learning_rate * max_future_q
 
             X.append(state)
             Y.append(current_qs)
@@ -138,11 +163,14 @@ class DQNModel:
     def play_game(self,
                   epsilon: float,
                   train: bool,
-                  show: bool) -> Tuple[bool, float,float,float]:
+                  show: bool,) -> Tuple[bool, float, float, float]:
         rewards = []
         loss_list = []
         accuracy_list = []
-        past_rewards= 23
+        observations = deque(maxlen=self.past_frames)
+        for _ in range(4):
+            observations.append(np.zeros((500,500,3)))
+        action_list = []
 
         pygame.init()
         if show:
@@ -179,6 +207,7 @@ class DQNModel:
         observations.append(state)
 
         game_over = False
+
         start_time = time.time()
         while not game_over:
             self.steps_to_update_target_model += 1
@@ -188,16 +217,17 @@ class DQNModel:
             else:
                 dt = 1000 / (15 * speed)
 
-            state = game.get_full_state_for_dqn(agent=game.player)
+            history_state = np.concatenate([o for o in observations], axis=2)
             if train:
                 if np.random.random() < epsilon:
                     action = np.random.choice(list(Action), 1, p=[0.17, 0.17, 0.17, 0.17, 0.15, 0.17])[0].value
                 else:
-                    preds = self.target_model.predict(np.expand_dims(state,axis=0), verbose=0)
+                    preds = self.target_model.predict(np.expand_dims(history_state, axis=0), verbose=0)
                     action = np.argmax(preds)
             else:
-                action = np.argmax(self.target_model.predict(np.expand_dims(state,axis=0),verbose=0))
+                action = np.argmax(self.target_model.predict(np.expand_dims(history_state, axis=0), verbose=0))
 
+            action_list.append(Action(action))
             # move player
             is_move_possible = game.player.move(action=Action(action),
                                                 grid=game.grid,
@@ -210,37 +240,35 @@ class DQNModel:
                 if not enemy.alive:
                     continue
                 tmp_state = game.get_state(agent=enemy,
-                                       state_type=enemy.state_type,
-                                       state_range=enemy.state_range,
-                                       min_enemy_dist=enemy.min_enemy_dist) if enemy.algorithm == Algorithm.Q else None
+                                           state_type=enemy.state_type,
+                                           state_range=enemy.state_range,
+                                           min_enemy_dist=enemy.min_enemy_dist) if enemy.algorithm == Algorithm.Q else None
                 enemy.choose_move(grid=game.grid,
                                   bombs=game.bombs,
                                   explosions=game.explosions,
                                   agents=game.agents_on_board,
                                   power_ups=game.power_ups,
-                                  state=tmp_state)
+                                  state=tmp_state,
+                                  surface=surface)
 
             # update bombs
             suicide, kills, destroyed_boxes = game.update_bombs(dt)
             game.draw(surface)
             observations.append(pygame.surfarray.array3d(surface))
 
+            before_training = time.time()
             # ----------------------------------------
+            reward = self.get_reward(game.player.alive,
+                                     Action(action),
+                                     is_move_possible,
+                                     suicide,
+                                     kills,
+                                     destroyed_boxes)
+            rewards.append(reward)
             if train:
-                reward = self.get_reward(game.player.alive,
-                                         Action(action),
-                                         is_move_possible,
-                                         suicide,
-                                         kills,
-                                         destroyed_boxes)
-                rewards.append(reward)
-                reward = np.sum([0.99**(past_rewards - i) * r for i, r in enumerate(rewards[-past_rewards:])])
-                if game.player.alive:
-                    future_state = game.get_full_state_for_dqn(agent=game.player)
-
-                    self.replay_memory.append([state, action, reward, future_state, game.player.alive])
-                else:
-                    self.replay_memory.append([state, action, reward, state, game.player.alive])
+                # reward = np.sum([0.98**(len(tmp_rewards) - i) * r for i, r in enumerate(tmp_rewards)])
+                fhistory_state = np.concatenate([o for o in observations], axis=2)
+                self.replay_memory.append([history_state, action, reward, fhistory_state, game.player.alive])
 
                 if self.steps_to_update_target_model % 4 or not game.player.alive:
                     loss, acc = self.train_model(self.replay_memory)
@@ -252,14 +280,31 @@ class DQNModel:
                     self.target_model.set_weights(self.main_model.get_weights())
                     self.steps_to_update_target_model = 0
 
+            after_training = time.time()
+
             # ----------------------------------------
             if show:
                 pygame.display.update()
 
             if not game_over:
-                game.playing_time = time.time() - start_time
+                game.playing_time = time.time() - start_time - (after_training - before_training)
                 game_over = game.check_end_game()
 
+            with open(self.log_file, 'a') as f:
+                dead_type = ''
+                if not game.player.alive:
+                    if suicide:
+                        dead_type = 'suicide'
+                    else:
+                        dead_type = 'killed by enemy'
+                move = green(str(Action(action)).rjust(20)) if is_move_possible else red(str(Action(action)).rjust(20))
+                if self.verbosity:
+                    print(f'{game.playing_time:3.3f} | {move:20} | {reward:3.3f} {rewards[-1]:3.3f} | '
+                          f'{destroyed_boxes:3} - {kills:3} | '
+                          f'{red("dead") if not game.player.alive else green("alive"):>5} {dead_type:>10}')
+                f.write(f'{game.playing_time:3.3f} | {str(Action(action)).rjust(20) if is_move_possible else str(Action(action)).rjust(20):20} | {reward:3.3f} {rewards[-1]:3.3f} | '
+                      f'{destroyed_boxes:3} - {kills:3} | '
+                      f'{red("dead") if not game.player.alive else green("alive"):>5} {dead_type:>10}\n')
         if show:
             pygame.display.quit()
             pygame.quit()
@@ -270,12 +315,12 @@ class DQNModel:
         mean_acc = 0 if accuracy_list is [] else float(np.mean(accuracy_list))
         mean_reward = 0 if rewards is [] else float(np.mean(rewards))
 
-        return player_win, mean_reward, mean_loss,mean_acc,
+        return player_win, mean_reward, mean_loss, mean_acc,
 
     def win_rate(self, n_games: int = 100) -> float:
         wins = 0
         for _ in tqdm(range(n_games), desc=f'Test on {n_games} games', unit='game'):
-            win, _, _, _ = self.play_game(0.01, False, False,)
+            win, _, _, _ = self.play_game(0.01, False, False, )
             if win:
                 wins += 1
         return wins / n_games
